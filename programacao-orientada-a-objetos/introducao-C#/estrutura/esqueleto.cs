3e855550ff4f9c58fa001eecf830a9152df27bd3
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-10-22 22:46:01 -0300

    Introdução a C#

---
+++ programacao-orientada-a-objetos/introducao-C#/estrutura/esqueleto.cs
@@ -0,0 +1,42 @@
+﻿using System;  // Inclui o namespace System, que contém classes e tipos úteis.
+
+namespace SeuNamespace  // Declara um namespace chamado SeuNamespace.
+{
+    class SuaClasse  // Declara uma classe chamada SuaClasse.
+    {
+        // Você pode adicionar membros e métodos dentro desta classe.
+    }
+
+    struct SuaEstrutura  // Declara uma estrutura chamada SuaEstrutura.
+    {
+        // Você pode adicionar campos e métodos dentro desta estrutura.
+    }
+
+    interface SuaInterface  // Declara uma interface chamada SuaInterface.
+    {
+        // Você pode declarar métodos que as classes devem implementar aqui.
+    }
+
+    delegate int SeuDelegado();  // Declara um delegado chamado SeuDelegado que representa um tipo de método.
+
+    enum SeuEnum  // Declara um enum chamado SeuEnum.
+    {
+        // Você pode definir valores enumerados aqui.
+    }
+
+    namespace SeuNamespaceDentroDaClasse  // Declara um namespace aninhado dentro de SeuNamespace.
+    {
+        struct SuaEstrutura  // Declara uma estrutura com o mesmo nome dentro deste namespace.
+        {
+            // Esta é uma estrutura diferente da SuaEstrutura definida no namespace pai.
+        }
+    }
+
+    class SuaClassePrincipal  // Declara uma classe chamada SuaClassePrincipal.
+    {
+        static void Main(string[] args)  // Método de ponto de entrada do programa.
+        {
+            // Seu programa começa a ser executado a partir deste método.
+        }
+    }
+}
---
+++ programacao-orientada-a-objetos/introducao-C#/programa.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace exemploCliente
+
+{
+
+class exemplo
+
+{
+
+public class Cliente
+
+{
+
+public string nome;
+public string sobrenome;
+public void EscreveNaConsole()
+{
+
+Console.WriteLine("Ola " + nome + " " + sobrenome);
+
+Console.WriteLine("Pressione qualquer tecla para sair");
+
+Console.ReadKey();
+
+}
+
+}
+
+static void Main()
+
+{
+
+Cliente objetoCliente = new Cliente();
+
+// Acessando as variáveis
+objetoCliente.nome = "Fulano";
+objetoCliente.sobrenome = "de tal";
+
+// Acessando o método
+objetoCliente.EscreveNaConsole();
+}
+}
+}
