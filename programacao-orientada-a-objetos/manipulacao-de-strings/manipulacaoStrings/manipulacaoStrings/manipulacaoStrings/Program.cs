1b64759da903f3a35516ed78a6a186f327be22a9
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-10-31 23:34:37 -0300

    Estudando strings

---
+++ programacao-orientada-a-objetos/manipulacao-de-strings/manipulacaoStrings/manipulacaoStrings/manipulacaoStrings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string no estado não inicializada
+        string naoIcicializada;
+
+        // string nula
+        string nula = null;
+        
+        // string com valor
+        string comValor = "Gabriel";
+
+        // vazia com empty
+        string vaziaComEmpty = System.String.Empty;
+
+        // vazia com ""
+        string vaziaComAspas = "";
+
+        /* em variáveis locais (dentro do corpo de um método)
+         * com o var, o compilador infere o tipo de dado (string, int..)*/
+        var texto = "Hello, World!";
+
+
+        /* usando o constructor string - usar quando:
+         * a string for um char*, char[], ou sbyte*
+        */
+        char[] letras = { 'A', 'B', 'C' };
+        string alfabeto = new string(letras);
+
+
+
+
+        // Usando o método String.Format
+        decimal temp = 22.0m;
+        string frase = String.Format("A temperatura é {0} ºC.", temp);
+        Console.WriteLine(frase); // A temperatura é 22,4 ºC.
+
+        // Ppressione uma tecla para sair
+        Console.ReadKey();
+    }
+}
---
+++ programacao-orientada-a-objetos/manipulacao-de-strings/metodosStrings/metodosStrings/metodosStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // método String.Format
+        decimal temp = 22.0m;
+        string texto = String.Format("A temperatura é {0} ºC.", temp);
+        // Saída de texto: A temperatura é 22,4 ºC.
+
+
+        // indexOf 
+        String frase = "Esta e uma string maior ainda para teste";
+        int tamanho = frase.Length -1;
+        int indiceInicial = 0;
+        string valor = "maior";
+        int posicao = frase.IndexOf(valor, indiceInicial, tamanho);
+        // Saída: 18, pois a subpalavra maior é encontrada na posição 18 da string
+
+
+        // String.Concat
+        string nome = "Gabriel";
+        string sobrenome = " Silva";
+        string nomeCompleto = String.Concat(nome,sobrenome);
+        // Gabreil Silva
+
+
+        // String.Join -> concatena os elementos de uma matriz usando um separador específico
+        String[] frutas = { "uva", "laranja", "pera", "abacaxi" };
+        String separador = ", ";
+        string frutasSeparadas = String.Join(separador, frutas);
+        // uva, laranja, pera, abacaxi
+    }
+}
