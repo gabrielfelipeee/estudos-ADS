e704a16c92d323ee7e8f01704450ef227a5eaa58
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-11-21 18:28:23 -0300

    Usando delgates

---
+++ programacao-orientada-a-objetos/delgates/delgates/delgates/Program.cs
@@ -0,0 +1,15 @@
+﻿public delegate void MyDelegate(); // Declaração do delgate
+
+class MainClass
+{ 
+    public static void MyMethod() // Declaração do método
+    {
+        Console.WriteLine("Hello, world!");
+    }
+
+    public static void Main(string[] args)
+    {
+        MyDelegate del1 = new MyDelegate(MyMethod); // Instância do delgate
+        del1();
+    }
+}
